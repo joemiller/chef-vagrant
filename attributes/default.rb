0741b46df561a5f7e56a6128a6dff41208362bd1
joe miller <joeym@joeym.net>
2012-05-04 09:16:18 -0700

    first commit of ::vagrant_from_package recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 # for details on how Oracle names the vbox packages see here:
 # https://www.virtualbox.org/wiki/Linux_Downloads
 node.default.vagrant.virtualbox_pkg_name = "virtualbox-4.1"
+
+node.default.vagrant.pkg_file = 'vagrant_1.0.3_x86_64.deb'
+node.default.vagrant.pkg_url = 'http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/vagrant_1.0.3_x86_64.deb'
---
+++ recipes/vagrant_from_package.rb
@@ -0,0 +1,13 @@
+
+vagrant_pkg_path = "#{Chef::Config['file_cache_path']}/#{node.vagrant.pkg_file}"
+
+remote_file vagrant_pkg_path do
+  source  node.vagrant.pkg_url
+  mode    0644
+end
+
+package "vagrant" do
+  action  :install
+  source  vagrant_pkg_path
+  provider Chef::Provider::Package::Dpkg
+end
